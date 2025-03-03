2e1833d19c001a98015abc06c49562ece12046ca
DarraghKennedy2008 <FARTDOO123456@gmail.com>
2025-03-03 19:09:07 +0000

    Test

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 <div className="flex items-center justify-center h-screen">
 <div className="size-64 rounded-lg border-2 border-zinc-200 bg-zinc-950">
 
-<img className="rounded-full" src="daisypic.jpg"></img>
+<img className="rounded-full align-bottom" src="daisypic.jpg"></img>
 
 </div></div>
 
